dba3ca6ad25438287828e2eb72470a8995968eab
Stephen Merendino <StephenMerendino@gmail.com>
2024-01-15 18:10:44 -0600

    Filling out app

--- sm-workbench/Source/App.cpp
+++ sm-workbench/Source/App.cpp
@@ -39,6 +39,8 @@ App::App()
 
 void App::Run()
 {
+	m_bIsRunning = true;
+
 	// TODO: Use SetProcessDpiAwarenessContext to make dpi awareness work per monitor
 	SetProcessDPIAware(); // make sure window is created with scaling handled
 
@@ -46,10 +48,40 @@ void App::Run()
 	appWindow->Init("SM Workbench", WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_ALLOW_RESIZE);
 	appWindow->AddMsgCallback(AppWindowMsgHandler);
 
-	m_bIsRunning = true;
 	while (m_bIsRunning)
 	{
 		appWindow->Update();
 		::Sleep(100);
+
+		// stats
+		F32 ds = stopwatch_get_elapsed_seconds(frame_stopwatch);
+		//report_fps(app_window, ds);
+
+		//// begin frame
+		//stopwatch_start(frame_stopwatch);
+		//input_begin_frame();
+		//renderer_begin_frame();
+
+		//////////////////////////
+		//game_begin_frame();
+		//////////////////////////
+
+		//// update
+		//window_update(app_window);
+		//input_update();
+		//camera_update(scene_camera, ds);
+		//renderer_update(ds);
+
+		//////////////////////////
+		//game_update(ds);
+		//game_render();
+		//////////////////////////
+
+		//// render
+		//renderer_render_frame();
+
+		//// end frame
+		//f32 frame_time_secs = stopwatch_get_elapsed_seconds(frame_stopwatch);
+		//sleep_remaining_frame(frame_time_secs);
 	}
 }
