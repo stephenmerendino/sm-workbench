e1ff023a00f2dcaa29b1b541638e5d65fb2ed83b
Stephen Merendino <StephenMerendino@gmail.com>
2024-02-05 16:30:53 -0600

    Working on a custom build step to build and deploy assets to run folder

---
+++ RawAssets/BuildAndDeployAssets.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set deployDir=%1
+
+REM @echo on
+REM echo %1
+
+REM set projectPath=%~dp0..
+REM set shaderPath=%projectPath%\assets\shaders
+REM 
+REM glslc %shaderPath%\tri.vert -o %shaderPath%\tri-vert.spv
+REM glslc %shaderPath%\tri.frag -o %shaderPath%\tri-frag.spv
+REM glslc %shaderPath%\simple-color.vert -o %shaderPath%\simple-color-vert.spv
+REM glslc %shaderPath%\simple-color.frag -o %shaderPath%\simple-color-frag.spv
+REM glslc %shaderPath%\pass-through.vert -o %shaderPath%\pass-through-vert.spv
+REM glslc %shaderPath%\post-process.frag -o %shaderPath%\post-process-frag.spv
