c2f4f0fb6f7e5aa83a4b0fcf872852513e3cc533
Stephen Merendino <StephenMerendino@gmail.com>
2024-02-05 21:19:52 -0600

    Accidentally gitignore obj files

--- RawAssets/BuildAndDeployAssets.bat
+++ RawAssets/BuildAndDeployAssets.bat
@@ -8,13 +8,17 @@ REM If %deployDir\RuntimeAssets\ does not exist, then create it
 if not exist %outputAssetDir% mkdir %outputAssetDir%
 
 REM Copy RawAssets\Textures\ to %deployDir\RuntimeAssets\Textures\
+echo Deploying textures to %outputAssetDir%Textures\
 if not exist %outputAssetDir%\Textures\ mkdir %outputAssetDir%\Textures\
-copy %rawAssetDir%Textures\ %outputAssetDir%Textures\ /y /d
+xcopy %rawAssetDir%Textures\ %outputAssetDir%Textures\ /s /d
 
 REM Copy RawAssets\Models\ to %deployDir\RuntimeAssets\Models\
+echo Deploying models to %outputAssetDir%Models\
+if not exist %outputAssetDir%\Models\ mkdir %outputAssetDir%\Models\
+xcopy %rawAssetDir%Models\ %outputAssetDir%Models\ /s /d
 
 REM Compile all .vert and .frag files to .spv files then copy them to %deployDir\RuntimeAssets\CompiledShaders\
-
+echo Compiling shaders
 
 
 
