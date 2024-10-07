2999bfa958779cda74af5be41e360f14df1133a5
Stephen Merendino <StephenMerendino@gmail.com>
2024-10-06 19:39:14 -0500

    Prototyping new engine utilities

--- sm-workbench/src/game.cpp
+++ sm-workbench/src/game.cpp
@@ -1,62 +1,79 @@
 #include "game.h"
 
+#include "sm/core/types.h"
+#include "sm/core/assert.h"
+#include "sm/memory/arena.h"
+
 #define WINDOWS_MEAN_AND_LEAN
 #include <windows.h>
 
-#include <cstdint>
-
-typedef		uint8_t		u8;
-typedef		uint16_t	u16;
-typedef		uint32_t	u32;
-typedef		uint64_t	u64;
-
-typedef		int8_t		i8;
-typedef		int16_t		i16;
-typedef		int32_t		i32;
-typedef		int64_t		i64;
-
-typedef		u8			byte_t;
-
-typedef void (*window_msg_cb)(UINT msg, WPARAM w, LPARAM l, void* user_args);
-struct window_msg_cb_with_args_t 
+// containers
+namespace sm
 {
-	window_msg_cb cb;
-	void* args;
-
-	bool operator==(const window_msg_cb_with_args_t& cb_with_args)
-	{
-		return cb == cb_with_args.cb && args == cb_with_args.args;
-	}
-};
-
-struct window_t
-{
-	HWND hwnd;
-	const char* name;
-	u32 width;
-	u32 height;
-	u32 x;
-	u32 y;
-	bool is_minimized;
-	bool was_resized;
-	bool was_closed;
-	bool is_moving;
-	//std::vector<WindowMsgCallbackWithArgs> m_msgCallbacks;
-	byte_t padding[4];
-};
-
-window_t* init_window()
-{
-	return nullptr;
+    // static_array
+    // dynamic_array
+    // ring_buffer
+    // queue
 }
+
+// string
+namespace sm
+{
+}
+
+// window
+namespace sm
+{
+    typedef void (*window_msg_cb)(UINT msg, WPARAM w, LPARAM l, void* user_args);
+    struct window_msg_cb_with_args_t 
+    {
+        window_msg_cb cb	= nullptr;
+        void* args			= nullptr;
+    };
+
+    bool operator==(const window_msg_cb_with_args_t& a, const window_msg_cb_with_args_t& b)
+    {
+        return a.cb == b.cb && a.args == b.args;
+    }
+
+    struct window_t
+    {
+        HWND hwnd;
+        const char* name;
+        u32 width;
+        u32 height;
+        u32 x;
+        u32 y;
+        bool is_minimized;
+        bool was_resized;
+        bool was_closed;
+        bool is_moving;
+        //std::vector<WindowMsgCallbackWithArgs> m_msgCallbacks;
+        byte_t padding[4];
+    };
+
+    window_t* init_window()
+    {
+        return nullptr;
+    }
+}
+
+#include <cstdio>
 
 int run_game()
 {
-	window_t* window = init_window();
+	sm::window_t* window = sm::init_window();
 
-	static_array<window_msg_cb_with_args_t> foo = init_static_array(50);
+    sm::memory_arena_t* app_arena = sm::arena_init(KiB(1));
 
-	dynamic_array<window_msg_cb_with_args_t> foo = init_dynamic_array(50);
+    char* s = (char*)arena_alloc_zero(app_arena, 50);
+    sprintf_s(s, 50, "%s\n", "Testing...");
+    printf("%s\n", s);
+
+    sm::arena_free(app_arena, 50);
+
+	//static_array<window_msg_cb_with_args_t> foo = init_static_array(app_arena, 50);
+	//dynamic_array<window_msg_cb_with_args_t> foo = init_dynamic_array(app_arena, 50);
 
 	return 0;
 }
