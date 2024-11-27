392233bbb4ca920dd91b8807aa2c31a47b3961f2
Stephen Merendino <StephenMerendino@gmail.com>
2024-11-27 17:08:14 -0600

    Update game.cpp

--- sm-workbench/src/game.cpp
+++ sm-workbench/src/game.cpp
@@ -10,17 +10,14 @@
 
 int run_game()
 {
-    sm::printf("size of ptr: %i\n", sizeof(char*));
-    sm::printf("size of size_t: %i\n", sizeof(size_t));
+    sm::debug_printf("size of ptr: %i\n", sizeof(char*));
+    sm::debug_printf("size of size_t: %i\n", sizeof(size_t));
 
     sm::arena_t* app_arena = sm::init_arena(KiB(1));
 
     sm::window_t* window = sm::init_window(app_arena, "SM Workbench", 1920, 1080, true);
 
-    //sm::static_array_t<sm::window_msg_cb_with_args_t> m_msg_cbs = sm::static_array_init<sm::window_msg_cb_with_args_t>(app_arena, 4);
-
-    //sm::window_msg_cb_with_args_t test_cb;
-    //m_msg_cbs[0] = test_cb;
+    size_t size_of_size = sizeof(size_t);
 
     sm::free(app_arena, 50);
 
