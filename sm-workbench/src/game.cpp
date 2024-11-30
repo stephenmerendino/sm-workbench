d3d9cd693f59b2776af05456724a88ec6ac55f12
Stephen Merendino <StephenMerendino@gmail.com>
2024-11-30 09:00:26 -0600

    Main looping again

--- sm-workbench/src/game.cpp
+++ sm-workbench/src/game.cpp
@@ -9,8 +9,10 @@
 #include "sm/core/types.h"
 #include "sm/memory/arena.h"
 #include "sm/render/window.h"
+#include "sm/thread/thread.h"
 
 static bool s_is_running = true;
+static sm::window_t* s_app_window = nullptr;
 
 static void report_fps(sm::f32 ds)
 {
@@ -24,14 +26,10 @@ static void report_fps(sm::f32 ds)
 	{
 		sm::f32 avg_current_fps = (sm::f32)s_frame_count / s_frame_time_accrual;
 
-	//	std::ostringstream ss;
-	//	ss << "SM Workbench" << " - FPS: " << avg_current_fps	 << " - Frame Time(ms): " << ds * 1000.0f;
-	//	std::string newTitle = ss.str();
-
 		char new_title[256];
-		//sprintf_s()
+		::sprintf_s(new_title, "SM Workbench - FPS: %f - Frame Time (ms): %f\n", avg_current_fps, ds * 1000.0f);
 
-		sm::set_title(nullptr, new_title);
+		sm::set_title(s_app_window, new_title);
 
 		s_frame_count = 0;
 		s_frame_time_accrual = 0.0f;
@@ -40,24 +38,24 @@ static void report_fps(sm::f32 ds)
 
 static void sleep_remaining_frame(sm::f32 ds)
 {
-	//const sm::f32 maxSecondsPerFrame = (1.0f / MAX_FPS);
+	const sm::f32 max_seconds_per_frame = (1.0f / MAX_FPS);
 
-	//// frame took longer than our max time
-	//if (ds >= maxSecondsPerFrame)
-	//{
-	//	return;
-	//}
+	// frame took longer than our max time
+	if (ds >= max_seconds_per_frame)
+	{
+		return;
+	}
 
-	//sm::f32 timeToSleepSeconds = maxSecondsPerFrame - ds;
-	//Thread::SleepSeconds(timeToSleepSeconds);
+	sm::f32 time_to_sleep_seconds = max_seconds_per_frame - ds;
+	sm::sleep_thread_seconds(time_to_sleep_seconds);
 }
 
 int run_game()
 {
     sm::arena_t* app_startup_arena = sm::init_arena(KiB(1));
 
-    sm::window_t* window = sm::init_window(app_startup_arena, "SM Workbench", 1920, 1080, true);
-    sm::set_title(window, "Test");
+    s_app_window = sm::init_window(app_startup_arena, "SM Workbench", 1920, 1080, true);
+    sm::set_title(s_app_window, "Test");
 
     sm::init_time();
 
@@ -66,7 +64,7 @@ int run_game()
 	sm::f32 ds = 0.0f;
 	while (s_is_running)
 	{
-        sm::update_window(window);
+        sm::update_window(s_app_window);
 
 		// End Frame
 		sm::f32 workTimeSeconds = sm::get_elapsed_seconds(&frame_stopwatch);
