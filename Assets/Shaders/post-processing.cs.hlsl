07f19665d80444d175be19f768f5eecbec3b23f1
Stephen Merendino <StephenMerendino@gmail.com>
2025-04-04 20:23:42 -0500

    Update post processing shader

--- Assets/Shaders/post-processing.cs.hlsl
+++ Assets/Shaders/post-processing.cs.hlsl
@@ -10,5 +10,5 @@ RWTexture2D<float4> g_post_processing_result : register(u1, space0);
 void main(uint3 dispatch_thread_id : SV_DispatchThreadID)
 {
     uint2 xy_pos = dispatch_thread_id.xy;
-    g_post_processing_result[xy_pos] = g_main_draw_result[xy_pos] * float4(1.0f, 1.0f, 0.0f, 1.0f);
+    g_post_processing_result[xy_pos] = g_main_draw_result[xy_pos] * float4(1.0f, 1.0f, 1.0f, 1.0f);
 }
