ca64947c90cd0af1ae65165bd80b8ad93f0580d5
Stephen Merendino <StephenMerendino@gmail.com>
2025-05-14 16:48:43 -0500

    Update todo and starting to think about game -> engine layer

--- sm-workbench/src/game/game.cpp
+++ sm-workbench/src/game/game.cpp
@@ -1,4 +1,16 @@
 #include "game.h"
+#include "sm/render/vk_renderer.h"
+
+//static sm::mesh_id_t s_viking_room_mesh_instance_id;
+//static sm::material_id_t s_viking_room_mesh_instance_id;
+static sm::mesh_instance_id_t s_viking_room_mesh_instance_id;
+
+void game_init()
+{
+	//renderer_load_mesh();
+	//renderer_load_material();
+	//renderer_add_mesh_instance();
+}
 
 void game_update(sm::f32 ds)
 {
--- sm-workbench/src/game/game.h
+++ sm-workbench/src/game/game.h
@@ -2,5 +2,6 @@
 
 #include "sm/core/types.h"
 
+void game_init();
 void game_update(sm::f32 ds);
 void game_render();
