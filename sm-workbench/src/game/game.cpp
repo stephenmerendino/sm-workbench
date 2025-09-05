6acba29942d45be2620ac6c234c3ed0d01f8eb7f
Stephen Merendino <StephenMerendino@gmail.com>
2025-09-05 11:54:04 -0500

    Setting up bones of game side code

--- sm-workbench/src/game/game.cpp
+++ sm-workbench/src/game/game.cpp
@@ -1,15 +1,28 @@
 #include "game.h"
+#include "sm/core/debug.h"
 #include "sm/render/vk_renderer.h"
+
+#include <string>
 
 //static sm::mesh_id_t s_viking_room_mesh_instance_id;
 //static sm::material_id_t s_viking_room_mesh_instance_id;
 static sm::mesh_instance_id_t s_viking_room_mesh_instance_id;
+
+static void tester()
+{
+}
 
 void game_init()
 {
 	//renderer_load_mesh();
 	//renderer_load_material();
 	//renderer_add_mesh_instance();
+	tester();
+}
+
+void game_begin_frame()
+{
+
 }
 
 void game_update(sm::f32 ds)
--- sm-workbench/src/game/game.h
+++ sm-workbench/src/game/game.h
@@ -3,5 +3,6 @@
 #include "sm/core/types.h"
 
 void game_init();
+void game_begin_frame();
 void game_update(sm::f32 ds);
 void game_render();
